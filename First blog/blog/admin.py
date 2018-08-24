53bd8a284735550a379014408655636943b5933e
xuanngoc <bx.ngoc.99@gmail.com>
2018-08-24 21:52:17 +0700

    delete

--- First blog/blog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Post
-# Register your models here.
-class PostAdmin(admin.ModelAdmin):
-	list_display =['title', 'date']
-	list_filter = ['date']
-	search_fields= ['title']
-admin.site.register(Post, PostAdmin)
--- First blog/home/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase, SimpleTestCase
-
-# Create your tests here.
-class SimpleTests(SimpleTestCase):
-	def test_home_page_status(self):
-		response = self.client.get('/')
-		self.assertEquals(response.status_code, 200)
